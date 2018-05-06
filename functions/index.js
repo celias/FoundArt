76f6abbe185bd4aeff2d2b28d278f02b34f2fbbd
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-05-06 13:17:57 -0500

    server

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
